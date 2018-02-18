2e0d8e40e10813008e8c04dc2f71acd9797aeed4
Tim Eccleston <teccleston@axialhealthcare.com>
2018-02-18 15:14:18 -0600

    first working version of triangle-duel

---
+++ triangle-duel/README.md
@@ -0,0 +1,4 @@
+# Triangle Duel
+
+## Citation
+Problem 24 on page 12 of Martin Gardner's "My Best Mathematical & Logic Puzzles"
---
+++ triangle-duel/__main__.py
@@ -0,0 +1,43 @@
+from fractions import Fraction as F
+from numpy import array as a
+import numpy as np
+
+p = {
+    "ca": a([F(1, 2), 0, F(1, 2)]),
+    "bc": a([0, F(8, 9), F(1, 9)]),
+    "cb": a([0, F(4, 9), F(5, 9)]),
+    "a": "ca",
+    "bac": ((F(4, 5), "cb"), (F(1, 5), "a")),
+    "cab": ((F(1, 2), "bc"), (F(1, 2), "a")),
+    "bca": ((F(4, 5), "cb"), (F(1, 5), "cab")),
+    "cba": ((F(1, 2), "bc"), (F(1, 2), "bac")),
+    "start": (
+        (F(1, 3), "a"),
+        (F(1, 6), "bac"),
+        (F(1, 6), "cab"),
+        (F(1, 6), "bca"),
+        (F(1, 6), "cba"),
+    )
+}
+
+def compute(entry_point_ref):
+    """computes probability for each (A, B, C) to be last survivor
+    """
+    entry_point = p[entry_point_ref]
+    if isinstance(entry_point, np.ndarray):
+        return entry_point
+    elif isinstance(entry_point, str):
+        return p[entry_point]
+    else:
+        results = [compute(pair[1]) * pair[0] for pair in entry_point]
+
+    return np.asarray(results).sum(axis=0)
+
+# import pdb; pdb.set_trace()
+compute("ca")
+compute("bc")
+compute("cb")
+compute("a")
+compute("bac")
+print compute("start")
+print [float(x) for x in compute("start")]
